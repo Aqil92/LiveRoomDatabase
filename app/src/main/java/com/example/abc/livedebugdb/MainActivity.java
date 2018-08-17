523ec30afd237622e96955d46d77196a48ab808a
Mohd Aqil <aqil.mohd.92@gmail.com>
2018-08-17 14:25:32 +0530

    Initial commit

---
+++ app/src/main/java/com/example/abc/livedebugdb/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.abc.livedebugdb;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        Log.v("","NO Main Activtiy Layout");
+    }
+}
